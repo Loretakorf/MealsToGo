99d2abc2c0e48518029240e06615836aa443e8e3
loretakorf <96435648+Loretakorf@users.noreply.github.com>
2023-06-30 16:07:05 +0300

    servises

---
+++ src/servises/restaurants/restaurants.servise.js
@@ -0,0 +1,24 @@
+import { mocks } from "./mock";
+import camelize from "camelize";
+
+export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
+  return new Promise((resolve, reject) => {
+    const mock = mocks[location];
+    if (!mock) {
+      reject("Not found");
+    }
+    resolve(mock);
+  });
+};
+const restaurantsTransform = (result) => {
+  const newResult = camelize(result);
+  return newResult;
+};
+restaurantsRequest()
+  .then(restaurantsTransform)
+  .tnen((transformedResponse) => {
+    console.log(transformedResponse);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
